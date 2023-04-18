d45767dc05f75c232d0ec17916d82370d00d1e1a
rancelazavedo <azavedorancel@gmail.com>
2023-04-18 23:36:37 +0530

    Second commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {faChevronRight, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import{ faFacebookF, faPinterest, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function Footer(){
+    return(
+        <section class="footer">
+
+    <div class="box-container reveal">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#"> <FontAwesomeIcon icon={faChevronRight}/> home </a>
+            <a href="#"> <FontAwesomeIcon icon={faChevronRight}/> services </a>
+            <a href="#"> <FontAwesomeIcon icon={faChevronRight}/> about </a>
+            <a href="#"> <FontAwesomeIcon icon={faChevronRight}/> doctors </a>
+            <a href="#"> <FontAwesomeIcon icon={faChevronRight}/> book </a>
+            <a href="#"> <FontAwesomeIcon icon={faChevronRight}/> review </a>
+            <a href="#"> <FontAwesomeIcon icon={faChevronRight}/> blogs </a>
+        </div>
+
+        <div class="box">
+            <h3>our services</h3>
+            <a href="#"> <FontAwesomeIcon class={faChevronRight}/> dental care </a>
+            <a href="#"> <FontAwesomeIcon class={faChevronRight}/> message therapy </a>
+            <a href="#"> <FontAwesomeIcon class={faChevronRight}/> cardioloty </a>
+            <a href="#"> <FontAwesomeIcon class={faChevronRight}/> diagnosis </a>
+            <a href="#"> <FontAwesomeIcon class={faChevronRight}/> ambulance service </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <FontAwesomeIcon class="faPhone"/> +123-456-7890 </a>
+            <a href="#"> <FontAwesomeIcon class="faPhone"/> +111-222-3333 </a>
+            <a href="#"> <FontAwesomeIcon class="faEnvelope"/> shaikhanas@gmail.com </a>
+            <a href="#"> <FontAwesomeIcon class="faEnvelope"/> anasbhai@gmail.com </a>
+            <a href="#"> <FontAwesomeIcon class="fas fa-map-marker-alt"/> mumbai, india - 400104 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <FontAwesomeIcon class="faFacebookF"/> facebook </a>
+            <a href="#"> <FontAwesomeIcon class="faTwitter"/> twitter </a>
+            <a href="#"> <FontAwesomeIcon class="faTwitter"/> twitter </a>
+            <a href="#"> <FontAwesomeIcon class="faInstagram"/> instagram </a>
+            <a href="#"> <FontAwesomeIcon class="faLinkedin"/> linkedin </a>
+            <a href="#"> <FontAwesomeIcon class="faPinterest"/> pinterest </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Rancel Azavedo</span> | all rights reserved </div>
+
+</section>
+    );
+}
--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -1,23 +1,12 @@
 import React from "react";
 import logo from "./logo2.png"
 import './Navbar.css'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react"
+import { useEffect, useState } from 'react';
 
 
 
-export function Profile(){
-    return(
-        <a class="logo-name"><img src={logo} class="logo"/>Victor Hospital</a>
-    );
-}
-
-export function Menubtn(){
-    return(
-        <FontAwesomeIcon icon={faBars} id="menu-btn"/>
-    );
-};
 
 export function Navbar(){
     return(
@@ -36,11 +25,34 @@ export function Navbar(){
 }
 
 export function Header(){
+    const [toggle, setToggle] = useState(true);    
+    const [navham, setNavham] = useState("navbar active")
+    useEffect(()=>{
+        if(toggle==true){
+            setNavham('navbar')
+        }
+        else{
+            setNavham('navbar active')
+        }
+        
+    });
+    console.log(toggle.valueOf)
     return(
-        <header class="header">
-            <Profile/>
-            <Navbar/>
-            <Menubtn/>
-        </header>
-    );
+        <div>
+            <div class="header">
+            <a class="logo-name"><img src={logo} class="logo"/>Victor Hospital</a>
+            <nav class={navham}>
+                <a href="#home">home</a>
+                <a href="#services">services</a>
+                <a href="#about">about</a>
+                <a href="#doctors">doctors</a>
+                <a href="#book">book</a>
+                <a href="#review">review</a>
+                <a href="#blogs">blogs</a>
+            </nav>
+            <button  id="menu-btn" onClick={()=>setToggle(!toggle)}><FontAwesomeIcon icon={faBars}/></button>
+            </div>
+        </div>
+        
+    )
 }
